1000f3897dda4c624b5dddd70827e07bf36ef947
Gerrel Atanacio <gerrelatanacio@gmail.com>
2023-07-25 13:50:31 +0800

    25JUL2023_1350 push with express package

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 console.log("Hello World");
 
-const { v4: uuidv4 } = require("uuid");
-
-// import { v4 as uuidv4 } from "uuid";
-//this pattern of using packages or modules is called Common JS
-//It allows us users to assign our modules functions to variable
-//Where the package-name is inside the close and open parenthesis right after the keyword, "require"
-
-console.log(uuidv4());
-//function call
+const express = require("express");
+const app = express();
+const PORT = 3000;
+app.get("/", (req, res) => {
+  res.send("HELLO WORLD FROM BROWSER");
+});
+app.listen(PORT, () => {
+  console.log("App listening on PORT: " + PORT);
+});
