38d09f0f02ee86636ddd8aa3533211db5d0a91b5
Gerrel Atanacio <gerrelatanacio@gmail.com>
2023-07-25 14:38:59 +0800

    25JUL2023_1438 installed nodemon package

--- src/app.js
+++ src/app.js
@@ -6,6 +6,11 @@ const PORT = 3000;
 app.get("/", (req, res) => {
   res.send("HELLO WORLD FROM BROWSER");
 });
+
+app.post("/", (req, res) => {
+  res.send("THIS IS A POST REQUEST 2.0");
+});
+
 app.listen(PORT, () => {
   console.log("App listening on PORT: " + PORT);
 });
