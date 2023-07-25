4471e8b6bf3a02af663781ac0fff902eb92c12a7
Gerrel Atanacio <gerrelatanacio@gmail.com>
2023-07-25 18:09:33 +0800

    25JUL2023_1808 added new express endpoints using get and post

--- src/app.js
+++ src/app.js
@@ -2,9 +2,48 @@ console.log("Hello World");
 
 const express = require("express");
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const PORT = 3000;
+
+const Customers = [
+  {
+    "name": "Gerrel Atanacio",
+    "industry": "Information Technology",
+    "occupation": "Software Engineer",
+    "age": 30,
+    "married": false,
+  },
+  {
+    "name": "Levin de Leon",
+    "industry": "Information Technology",
+    "occupation": "Software Engineer",
+    "age": 30,
+    "married": false,
+  },
+  {
+    "name": "Rosem Karl Palo",
+    "industry": "Information Technology",
+    "occupation": "Software Engineer",
+    "age": 31,
+    "married": false,
+  },
+];
+
 app.get("/", (req, res) => {
-  res.send("HELLO WORLD FROM BROWSER");
+  res.send("Welcome to HOME");
+});
+
+app.get("/api/customers", (req, res) => {
+  res.send({ customers: Customers });
+});
+
+app.post("/api/customers", (req, res) => {
+  console.log(req.body);
+  //shows not on the browser console but on the server console
+  res.send(req.body);
 });
 
 app.post("/", (req, res) => {
